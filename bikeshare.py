539603e3752720f1fad25a2b99a9fa78565a7584
Vivek Kaushik <vivekr09@gmail.com>
2021-07-07 09:08:00 -0400

    Modify user stat section to improve readability

--- bikeshare.py
+++ bikeshare.py
@@ -175,7 +175,7 @@ def trip_duration_stats(df):
 
 
 def user_stats(df):
-    """Displays statistics on bikeshare users."""
+    """Displays statistics on bikeshare users. It provides information regarding user's gender and birth year"""
 
     print('\nCalculating User Stats...\n')
     start_time = time.time()
